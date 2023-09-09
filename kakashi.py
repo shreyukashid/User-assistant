b253883d93308923611f6202015518eee6860d72
Jackantisepticeye <72183002+shreyukashid@users.noreply.github.com>
2023-09-09 05:45:28 -0700

    Create kakashi.py

---
+++ kakashi.py
@@ -0,0 +1,123 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+import pywintypes
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+        print("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")
+        print("Good Afternoon")   
+
+    else:
+        speak("Good Evening!")
+        print(("Good Evening!"))  
+
+    speak("I am Kakashi. Please tell me how may I help you")
+    print("I am Kakashi. Please tell me how may I help you")       
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('kashidshreyash10.cool@gmail.com', 'Shreyash2000')
+    server.sendmail('kashidshreyash10.cool@gmail.com', to, content)
+    server.close()
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")   
+
+
+        #elif 'play music' in query:
+            music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
+            songs = os.listdir(music_dir)
+            print(songs)    
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f" the time is {strTime}")
+
+        elif 'email to shreyu' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "kashidshreyash10.cool@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry. I am not able to send this email")
+        
+        elif 'my birthday' in query:
+            speak("your birthday is on 14 November")
+            print("your birthday is on 14 November")
+        
+        elif 'birthday of my mom' in query:
+            speak("its 23 november")
+            print("its 23 november")
+        
+        elif 'my favourite food' in query:
+            speak("your favourite food is pizza")
+            print("your favourite food is pizza")
